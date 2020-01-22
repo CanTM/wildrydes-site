518b7ab464e0bb1a44508d23e2a0a9ac604450a9
CanTM <candice.michelon@acad.pucrs.br>
2020-01-22 10:56:36 -0300

    Add region

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_ajSgJoDCy', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'eh9ij81amnjrglcqoln8o8o49', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
